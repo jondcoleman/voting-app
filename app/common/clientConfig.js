85ec91a696042907d94ca6c37feeca902e81060d
Jonathan Coleman <jcoleman3307@gmail.com>
2015-11-10 13:22:09 +0000

    url changes

--- app/common/clientConfig.js
+++ app/common/clientConfig.js
@@ -1,4 +1,4 @@
 'use strict'
 
-var appUrl = 'https://basejump2-jondcoleman.c9.io/';
-//var appUrl = 'https://whispering-ocean-6392.herokuapp.com/';
+//var appUrl = 'https://basejump2-jondcoleman.c9.io/';
+var appUrl = 'https://agile-sierra-7023.herokuapp.com/';
